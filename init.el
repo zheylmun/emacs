5161342de58217602fc5419e0b8cf033758e14a7
Zach Heylmun <zheylmun@gmail.com>
2021-10-01 22:29:54 -0400

    First pass emacs config

---
+++ init.el
@@ -0,0 +1,40 @@
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(set-fringe-mode 10)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 160)
+(load-theme 'tango-dark)
+
+
+; Load package management functionality
+(require 'package)
+
+; Set some repositories
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+(use-package ivy
+  :diminish
+  :config (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
