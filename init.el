dbf3ecb8bb7809926469a63759dfacc4fadcb81b
Zach Heylmun <zheylmun@gmail.com>
2021-10-04 13:55:41 -0400

    add line numbers, rainbow delimiters, which key

--- init.el
+++ init.el
@@ -2,6 +2,16 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (set-fringe-mode 10)
+
+(column-number-mode)
+
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 160)
 (load-theme 'tango-dark)
 
@@ -36,5 +46,13 @@
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :custom ((doom-modeline-height 16)))
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish(which-key-mode)
+  :config
+  (setq which-key-idle-delay 0.3))
